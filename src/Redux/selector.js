d716833e759659c0a82eacdb18b88560a7971c30
Aryo Romadhon <yoomadhon@gmail.com>
2023-05-20 13:28:29 +0700

    update

--- src/Redux/selector.js
+++ src/Redux/selector.js
@@ -4,7 +4,7 @@ export const getTodosByVisibilityFilter = (store, visibilityFiler) => {
   switch(visibilityFiler) {
     case VISIBILITY_FILTER.COMPLETED:
       return store.todos.filter(todo => todo.completed);
-    case VISIBILITY_FILTER.INCOMPLETED:
+    case VISIBILITY_FILTER.ACTIVE:
       return store.todos.filter(todo => !todo.completed);
     default:
       return store.todos;
